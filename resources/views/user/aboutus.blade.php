0d2ea00c43e03cad25cf9078322af2f7ee8d95c7
akhmadali1 <akhmadaliernawan@gmail.com>
2022-06-23 16:49:51 +0700

    Initial commit

---
+++ resources/views/user/aboutus.blade.php
@@ -0,0 +1,124 @@
+<x-layouts>
+  <!-- Page Content -->
+  <div class="page-heading about-heading header-text">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="text-content">
+            <h4>about us</h4>
+            <h2>Who Are We?</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="best-features about-features">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-heading">
+            <h2>Our Background</h2>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="right-image">
+            <img src="assets/images/kado.jpg" alt="">
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="left-content">
+            <h4>Who we are &amp; What we do?</h4>
+            <p>We are Kadoin<br><br>Kami akan memudahkan kalian dalam mencari kado untuk yang kalian kasih dengan secara online.</p>
+            <ul class="social-icons">
+              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+              <li><a href="#"><i class="fa fa-behance"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+  <div class="team-members">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-heading">
+            <h2>Our Team Members</h2>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="team-member">
+            <div class="thumb-container">
+              <img src="assets/images/ali.jpg" alt="">
+              <div class="hover-effect">
+                <div class="hover-content">
+                  <ul class="social-icons">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="down-content">
+              <h4>Akhmad Ali Ernawan</h4>
+              <span>CO-Founder</span>
+              <p>Co-Founder of Kadoin</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="team-member">
+            <div class="thumb-container">
+              <img src="assets/images/irvan.jpg" alt="">
+              <div class="hover-effect">
+                <div class="hover-content">
+                  <ul class="social-icons">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="down-content">
+              <h4>Irvan Wijaya</h4>
+              <span>Product Expert</span>
+              <p>Product Expert at Kadoin.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="team-member">
+            <div class="thumb-container">
+              <img src="assets/images/axcel.jpg" alt="">
+              <div class="hover-effect">
+                <div class="hover-content">
+                  <ul class="social-icons">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="down-content">
+              <h4>Axcel Deon Davelin Syahputra</h4>
+              <span>Chief Marketing</span>
+              <p>Chief Marketing at Kadoin</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+</x-layouts>
